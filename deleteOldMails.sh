cac37a510e082b7fb65ded313a29bdec08b89178
czPechy <martin@pecha.pro>
2018-08-12 02:40:50 +0200

    Create deleteOldMails.sh

---
+++ deleteOldMails.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Trash - default
+find /home/*/mail/*/*/.Trash/cur -type f -mtime +30 -delete
+
+# Deleted Items - client ?
+find /home/*/mail/*/*/.Deleted\ Items/cur -type f -mtime +30 -delete
+
+# Deleted Messages - Roundcube
+find /home/*/mail/*/*/.Deleted\ Messages/cur -type f -mtime +30 -delete
+
+# Deleted Messages - Spam
+find /home/*/mail/*/*/.Spam/cur -type f -mtime +30 -delete
+
+# Deleted Messages - Junk
+find /home/*/mail/*/*/.Junk/cur -type f -mtime +30 -delete
