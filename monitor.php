93b05cfcc4d44d4ea65fcd98b25c33cb81c120cd
Martin Pecha <martin@pecha.pro>
2018-09-12 01:56:38 +0200

    next moduls

--- monitor.php
+++ monitor.php
@@ -1,18 +1,31 @@
 <?php
+$basic = host();
 $memory = memory();
 $disks = disk();
 $sensors = sensors();
+$iostat = iostat();
+$cpu = cpu();
 
 echo 'TESTING MODE:' . PHP_EOL;
 echo PHP_EOL;
 
-echo 'BOOT:' . PHP_EOL;
-echo ' - ' . (new DateTime())->setTimestamp($memory['boot'])->format('Y-m-d H:i:s') . PHP_EOL;
-echo ' - ' . bootTime(time() - $memory['boot']) . PHP_EOL;
+echo 'SYSTEM:' . PHP_EOL;
+echo ' - host:   ' . $basic['Static hostname'] . PHP_EOL;
+echo ' - os:     ' . $basic['Operating System'] . PHP_EOL;
+echo ' - kernel: ' . $basic['Kernel'] . PHP_EOL;
+echo ' - id:     ' . $basic['Machine ID'] . PHP_EOL;
+echo ' - boot:   ' . (new DateTime())->setTimestamp($memory['boot'])->format('Y-m-d H:i:s') . PHP_EOL;
+echo ' - uptime: ' . bootTime(time() - $memory['boot']) . PHP_EOL;
+
 echo PHP_EOL;
 
 echo 'CPU:' . PHP_EOL;
-echo ' - used:  ' . number_format(cpu(), 1) . '%' . PHP_EOL;
+echo ' - used:  ' . number_format($cpu, 1) . '%' . PHP_EOL;
+echo ' - stats: ' . PHP_EOL;
+echo '   - system: ' . number_format($iostat['cpu']['system'], 2) . '%' . PHP_EOL;
+echo '   - user:   ' . number_format($iostat['cpu']['user'], 2) . '%' . PHP_EOL;
+echo '   - idle:   ' . number_format($iostat['cpu']['idle'], 2) . '%' . PHP_EOL;
+echo ' - temp:  ~' . number_format(avgTemp($sensors),0) . '°C' . PHP_EOL;
 echo PHP_EOL;
 
 echo 'MEMORY:' . PHP_EOL;
@@ -26,6 +39,12 @@ echo ' - used:  ' . number_format(($memory['swap']['used']/$memory['swap']['tota
 echo PHP_EOL;
 
 echo 'DISK:' . PHP_EOL;
+foreach($iostat['disk'] as $disk) {
+    echo ' - ' . $disk['disk'] . PHP_EOL;
+    echo '   - stats:' . PHP_EOL;
+    echo '     - tps: ' . $disk['tps'] . PHP_EOL;
+    echo '     - r/w: ' . $disk['read_s'] . '/' . $disk['write_s'] . PHP_EOL;
+}
 foreach($disks as $disk) {
     echo ' - ' . $disk['disk'] . ':' . PHP_EOL;
     echo '   - total: ' . formatBytes($disk['total']) . PHP_EOL;
@@ -49,17 +68,17 @@ function bootTime($input) {
     $hrs = null;
 
     if($input > 60) {
-        $sec = str_pad($input % 60, 2, ' ', STR_PAD_LEFT);
+        $sec = str_pad($input % 60, 2, '0', STR_PAD_LEFT);
         $input = round($input/60);
         $unit = 'm';
     }
     if($input > 60) {
-        $mins = str_pad($input % 60, 2, ' ', STR_PAD_LEFT);
+        $mins = str_pad($input % 60, 2, '0', STR_PAD_LEFT);
         $input = round($input/60);
         $unit = 'h';
     }
     if($input > 24) {
-        $hrs = str_pad($input % 24, 2, ' ', STR_PAD_LEFT);
+        $hrs = str_pad($input % 24, 2, '0', STR_PAD_LEFT);
         $input = round($input/24);
         $unit = 'd';
     }
@@ -82,6 +101,58 @@ function formatBytes($input) {
         $unit = 'TB';
     }
     return number_format($input, 2) . ' ' . $unit;
+}
+
+function avgTemp($sensors) {
+    $tmpTemp = 0;
+    $tmpItems = 0;
+    foreach($sensors as $group) {
+        foreach($group as $core) {
+            $tmpTemp += $core['temp'];
+            $tmpItems++;
+        }
+    }
+    return $tmpTemp/$tmpItems;
+}
+
+function host() {
+    $result = shell_exec('hostnamectl');
+    preg_match_all('~^\s*([a-zA-Z0-9\s]+):\s(.*)$~m', $result, $matches);
+
+    $data = [];
+    foreach($matches[0] as $key => $item) {
+        $data[$matches[1][$key]] = $matches[2][$key];
+    }
+    return $data;
+}
+
+function iostat() {
+    $result = shell_exec('iostat');
+    preg_match('~\s+(?<user>[\d\,]+)\s+(?<nice>[\d\,]+)\s+(?<system>[\d\,]+)\s+(?<iowait>[\d\,]+)\s+(?<steal>[\d\,]+)\s+(?<idle>[\d\,]+)~', $result, $matches);
+    $cpu = [
+        'user' => toFloat($matches['user']),
+        'nice' => toFloat($matches['nice']),
+        'system' => toFloat($matches['system']),
+        'iowait' => toFloat($matches['iowait']),
+        'steal' => toFloat($matches['steal']),
+        'idle' => toFloat($matches['idle'])
+    ];
+    preg_match_all('~^(?<disk>[\da-zA-Z\,]+)\s+(?<tps>[\d\,]+)\s+(?<read_s>[\d\,]+)\s+(?<write_s>[\d\,]+)\s+(?<read>[\d\,]+)\s+(?<write>[\d\,]+)$~m', $result, $matches);
+    $disks = [];
+    foreach($matches['disk'] as $key => $disk) {
+        $disks[] = [
+            'disk' => $disk,
+            'tps' => toFloat($matches['tps'][$key]),
+            'read_s' => toFloat($matches['read_s'][$key]),
+            'read' => toFloat($matches['read'][$key]),
+            'write_s' => toFloat($matches['write_s'][$key]),
+            'write' => toFloat($matches['write'][$key]),
+        ];
+    }
+    return [
+        'cpu' => $cpu,
+        'disk' => $disks
+    ];
 }
 
 function memory() {
@@ -158,6 +229,9 @@ function sensors() {
 
 function cpu() {
 	$cpu = shell_exec('top -b -n 1 | grep "Cpu(s)\:" | awk \'{print $2}\'');
-	$cpu = str_replace(',', '.', $cpu);
-	return (float) $cpu;
+	return toFloat($cpu);
+}
+
+function toFloat($input) {
+    return (float) str_replace(',', '.', $input);
 }
